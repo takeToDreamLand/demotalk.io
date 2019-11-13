e82013112ba2eb8e2afe9c02ac7ed752dcfedc6c
Shaw <dreameameam@163.com>
2019-11-13 16:37:44 +0800

    Change: Change repo info

--- gitalk_config.js
+++ gitalk_config.js
@@ -3,7 +3,7 @@ if(!gitalk)
     var gitalk = new Gitalk({
         clientID: '8467726bf36fa0a756e5',
         clientSecret: '796e869f94f37108da19078471a255b570cfdd91',
-        repo: 'https://github.com/takeToDreamLand/demotalk.io.git',
+        repo: 'demotalk.io',
         owner: 'takeToDreamLand',
         admin: ['takeToDreamLand'],
         id: location.pathname,
