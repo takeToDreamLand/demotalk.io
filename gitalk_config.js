2e1d92d103e7b87f7cbecd92a0d4407f7a9185cb
Shaw <dreameameam@163.com>
2019-11-13 13:55:15 +0800

    Change:

--- gitalk_config.js
+++ gitalk_config.js
@@ -1,6 +1,6 @@
 if(gitalk)
 {
-    let gitalk = new Gitalk({
+    var gitalk = new Gitalk({
         clientID: '8467726bf36fa0a756e5',
         clientSecret: '796e869f94f37108da19078471a255b570cfdd91',
         repo: 'https://github.com/takeToDreamLand/demotalk.io.git',
@@ -9,7 +9,5 @@ if(gitalk)
         id: windows.location.pathname,
         distractionFreeMode: false
     });
-    gitalk.render('gitalk-container')
-}else{
-    gitalk.render('gitalk-container')
 }
+gitalk.render('gitalk-container');
