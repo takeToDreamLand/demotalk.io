fc6a4e3414c0c35fdda540ea9e016120133694bb
Shaw <dreameameam@163.com>
2019-11-13 17:15:05 +0800

    Revert "Change: Change repo info"
    This reverts commit e82013112ba2eb8e2afe9c02ac7ed752dcfedc6c.

--- gitalk_config.js
+++ gitalk_config.js
@@ -3,7 +3,7 @@ if(!gitalk)
     var gitalk = new Gitalk({
         clientID: '8467726bf36fa0a756e5',
         clientSecret: '796e869f94f37108da19078471a255b570cfdd91',
-        repo: 'demotalk.io',
+        repo: 'https://github.com/takeToDreamLand/demotalk.io.git',
         owner: 'takeToDreamLand',
         admin: ['takeToDreamLand'],
         id: location.href,
