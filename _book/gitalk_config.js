bbafb05035517b231653e314a6ea09a3e9799697
Shaw <dreameameam@163.com>
2019-11-13 11:33:53 +0800

    Style: const to let, avoid the exist identifier

--- _book/gitalk_config.js
+++ _book/gitalk_config.js
@@ -1,5 +1,5 @@
 
-const gitalk = new Gitalk({
+let gitalk = new Gitalk({
     clientID: '8467726bf36fa0a756e5',
     clientSecret: '796e869f94f37108da19078471a255b570cfdd91',
     repo: 'https://github.com/takeToDreamLand/demotalk.io.git',
