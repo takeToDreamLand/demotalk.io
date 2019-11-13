c25e564c3bcb2554c262e72f189eddbe743737ba
Shaw <dreameameam@163.com>
2019-11-13 17:19:51 +0800

    Complete

--- gitalk_config.js
+++ gitalk_config.js
@@ -3,10 +3,10 @@ if(!gitalk)
     var gitalk = new Gitalk({
         clientID: '8467726bf36fa0a756e5',
         clientSecret: '796e869f94f37108da19078471a255b570cfdd91',
-        repo: 'https://github.com/takeToDreamLand/demotalk.io.git',
+        repo: 'demotalk.io.git',
         owner: 'takeToDreamLand',
         admin: ['takeToDreamLand'],
-        id: location.href,
+        id: location.pathname,
         distractionFreeMode: false
     });
 }
