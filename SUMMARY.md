50e865e8d8fd79a59a380d07c93253e25a28dc19
Shaw <dreameameam@163.com>
2019-11-20 11:44:27 +0800

    Add getSitemap and token and urls.js

--- SUMMARY.md
+++ SUMMARY.md
@@ -6,3 +6,6 @@
       * [Post2](chapter1/post2.md)
     * [Chapter2]()
       * [Post1](chapter2/post1.md)
+      * [Post2](chapter2/post2.md)
+    * [Chapter3]()
+      * [Post1](chapter3/post1.md)
---
+++ getSitemap.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+fs.readFile('./SUMMARY.md', 'utf8', (err, str)=>{
+    if (err) throw err;
+    else {
+        let regex = /\((.*?)\.md\)/g;
+        console.log(regex);
+        let results = str.match(regex);
+        let urls = results.map((result) => {
+            return result.replace(/[\(\)]|\.md/g,'')
+        })
+        fs.writeFileSync('./urls.json',JSON.stringify(urls), (err) => {
+            console.log('Dumped news bits to file');})
+    }
+    
+})
