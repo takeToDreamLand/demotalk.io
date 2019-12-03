59521a4747656328f0577f877dd566ae67842b9e
Shaw <dreameameam@163.com>
2019-12-03 08:50:10 +0800

    Test Connection

--- getSitemap.js
+++ getSitemap.js
@@ -3,11 +3,11 @@ const fs = require('fs')
 fs.readFile('./SUMMARY.md', 'utf8', (err, str)=>{
     if (err) throw err;
     else {
-        let regex = /\((.*?)\.md\)/g;
+        let regex = /\]\((.*?)\.md\)/g;
         console.log(regex);
         let results = str.match(regex);
         let urls = results.map((result) => {
-            return result.replace(/[\(\)]|\.md/g,'')
+            return result.replace(/[\(\)\]]|\.md/g,'')
         })
         fs.writeFileSync('./urls.json',JSON.stringify(urls), (err) => {
             console.log('Dumped news bits to file');})
